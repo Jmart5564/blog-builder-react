ecd2a1a001515f00544a44e595a2b16d2a1f7e08
Jmart5564 <Jmart5564@gmail.com>
2022-09-14 12:07:40 -0700

    text state set and updates onChange

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,12 +8,19 @@ import './Home.css';
 export default function Home() {
   const [title, setTitle] = useState('');
   const [subtitle, setSubtitle] = useState('');
+  const [text, setText] = useState('');
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview {...{ title, subtitle }} />
-      <Editor {...{ title, setTitle, subtitle, setSubtitle }}/>
+      <Preview {...{ title, subtitle, text }} />
+      <Editor {...{
+        title, 
+        setTitle, 
+        subtitle, 
+        setSubtitle, 
+        text, 
+        setText }}/>
     </main>
   );
 }
--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import './Preview.css';
 
-export default function Preview({ title, subtitle }) {
+export default function Preview({ title, subtitle, text }) {
   return (
     <div>
       <h1>{title}</h1>
       <h3>{subtitle}</h3>
-      <p>This is some blog text. </p>
+      <p>{text}</p>
     </div>
   );
 }
