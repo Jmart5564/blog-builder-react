db9034d6b85be4665869c3694a7d470be75fa3cf
Jmart5564 <Jmart5564@gmail.com>
2022-09-14 12:02:29 -0700

    subtitle state set and updates onChange

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,12 +7,13 @@ import './Home.css';
 
 export default function Home() {
   const [title, setTitle] = useState('');
+  const [subtitle, setSubtitle] = useState('');
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview {...{ title }} />
-      <Editor {...{ title, setTitle }}/>
+      <Preview {...{ title, subtitle }} />
+      <Editor {...{ title, setTitle, subtitle, setSubtitle }}/>
     </main>
   );
 }
--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './Preview.css';
 
-export default function Preview({ title }) {
+export default function Preview({ title, subtitle }) {
   return (
     <div>
       <h1>{title}</h1>
-      <h3> Blog Subtitle</h3>
+      <h3>{subtitle}</h3>
       <p>This is some blog text. </p>
     </div>
   );
